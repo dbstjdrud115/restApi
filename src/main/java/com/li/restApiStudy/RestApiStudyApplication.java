52b206eb01f825af4f09bb8dc3168dc4cd1215e0
Ojiner <dbstjdrud115@gmail.com>
2025-01-04 17:50:56 +0900

    타 repo 데이터 commit가능여부 테스트

---
+++ src/main/java/com/li/restApiStudy/RestApiStudyApplication.java
@@ -0,0 +1,13 @@
+package com.li.restApiStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiStudyApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(RestApiStudyApplication.class, args);
+	}
+}
